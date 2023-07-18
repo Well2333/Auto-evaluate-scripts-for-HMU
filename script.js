712b5b4ea0c58e794da4cd26d2c844b411650a63
Well404 <40534114+Well2333@users.noreply.github.com>
2023-07-18 14:35:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动评教
 // @namespace    https://github.com/Well2333/Auto-evaluate-scripts-for-HMU
-// @version      0.1
+// @version      0.2
 // @description  自动评教脚本，适用于哈尔滨医科大学教务系统
 // @author       Well404
 // @match        https://jwweb.hrbmu.edu.cn/new/welcome.page
@@ -10,54 +10,78 @@
 // ==/UserScript==
 
 
-(function() {
-    function sleep(time){
+(function () {
+    function sleep(time) {
         return new Promise((resolve) => setTimeout(resolve, time));
-       }
-    async function main() {
-        await sleep(2000);
-        document.querySelector("#sm3").click();
-        await sleep(1000);
-        document.querySelector("#\\34 0 > table > tbody > tr:nth-child(1) > td").click();
-        await sleep(3000);
-        var sNum = 0;
-        var iframe_selector = document.querySelector('#nav_tab > div.tabs-panels.tabs-panels-noborder > div:nth-child(2) > div > iframe').contentWindow;
-        var comment_flag = null
-        while (true){
-            comment_flag = iframe_selector.document.querySelector("#datagrid-row-r1-2-" + sNum + " > td:nth-child(6) > div > a > span > span").textContent
-            try{
-                if (comment_flag== '进入评价') {
-                    iframe_selector.document.querySelector("#datagrid-row-r1-2-" + sNum + " > td:nth-child(6) > div > a").click();
-                    sNum += 1;
-                } else if (comment_flag == '已评价'){
-                    sNum += 1;
-                    continue;
-                } else if (comment_flag == null){
-                    alert('评教已完成');
+    }
+
+    function insertButton() {
+        var parentElement = document.querySelector('#header > div.infoDiv > div.bottom');
+        var newButton = document.createElement('a');
+        newButton.href = "javascript:void(0)";
+        newButton.innerText = '开始评教';
+        newButton.style.color = 'red';  // 使字体颜色变为红色
+        parentElement.appendChild(newButton);
+        newButton.addEventListener('click', async function () {
+            await main();
+        });
+        alert("自动评教脚本已加载，请先手动切换到评教页面，然后再点击“开始评教”按钮，脚本便会自动在此页面评教（不会自动翻页）");
+    }
+
+    async function evaluateAndSubmit(iframeDoc) {
+        var i = 1;
+        while (true) {
+            var radioButton = iframeDoc.querySelector("#container > div:nth-child(" + i + ") > input:nth-child(2)");
+            if (radioButton) {
+                radioButton.click();
+                i++;
+            } else {
+                var nextElement = iframeDoc.querySelector("#container > div:nth-child(" + i + ")");
+                if (nextElement) {
+                    i++;
+                } else {
                     break;
                 }
-            } catch(err) {
-                alert("脚本出现错误。错误描述：\n\n"+ err.message + "\n\n点击确定继续。");
             }
-            await sleep(3000);
-            var qnum = 1;
-            var panel_selector = document.querySelector('#nav_tab > div.tabs-panels.tabs-panels-noborder > div:nth-child(2) > div > iframe').contentDocument.querySelector("#searchpage > div.panel.window");
-            var ques_selector = panel_selector.querySelector("#container > div:nth-child("+qnum+") > input:nth-child(2)");
-            try{
-                while (true){
-                    if (ques_selector){
-                        qnum += 1;
-                        ques_selector.click();
-                        ques_selector = panel_selector.querySelector("#container > div:nth-child("+qnum+") > input:nth-child(2)");
-                    } else {
-                        break;
-                    }
-                }
-            } catch(err) {
-                alert("脚本出现错误。错误描述：\n\n"+ err.message + "\n\n点击确定继续。");
-            }
-            ques_selector = panel_selector.querySelector("#dlg > div > div > div > div.panel.layout-panel.layout-panel-south > div > div > a").click();
+            await sleep(10);  // 适当地调整等待时间
+        }
+
+        var submitButton = iframeDoc.querySelector("#dlg > div > div > div > div.panel.layout-panel.layout-panel-south > div > div > a > span > span");
+        if (submitButton) {
+            submitButton.click();
+            return true;
+        } else {
+            return false;
         }
     }
-    main();
+
+    async function main() {
+        var frames = document.getElementsByTagName('iframe');
+        for (var i = 0; i < frames.length; i++) {
+            try {
+                var buttons = frames[i].contentWindow.document.querySelectorAll('a');
+                for (var j = 0; j < buttons.length; j++) {
+                    if (buttons[j].textContent == '进入评价') {
+                        try {
+                            buttons[j].click();
+                            await sleep(1000);  // 适当地调整等待时间
+                            var evaluated = await evaluateAndSubmit(frames[i].contentWindow.document);
+                            if (!evaluated) {
+                                throw new Error('无法完成评价或点击提交按钮');
+                            }
+                        } catch (err) {
+                            alert("脚本出现错误。错误描述：\n\n" + err.message + "\n\n点击确定继续。");
+                        }
+                    }
+                }
+            } catch (err) {
+                // 忽略跨域iframe
+            }
+        }
+        alert('评教已完成');
+    }
+
+    window.onload = function () {
+        insertButton();
+    }
 })();
